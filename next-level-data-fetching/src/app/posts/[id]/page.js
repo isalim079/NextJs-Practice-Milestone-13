9fac1cac9ed540c7f78c88e6590d3864deb97edc
isalim079 <salimreza1025@gmail.com>
2023-12-10 18:39:08 +0600

    data fetching by id done and details route added

---
+++ next-level-data-fetching/src/app/posts/[id]/page.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const DetailPage = async({params}) => {
+
+    // console.log(params?.id);
+
+    const res = await fetch(`http://localhost:4008/posts/${params?.id}`);
+    const post = await res.json()
+    // console.log(post);
+
+    return (
+        <div className=' flex justify-center items-center h-screen'>
+            <div key={post?.id} className="card w-[720px] bg-cyan-800 shadow-xl text-white">
+                        <div className="card-body">
+                            <h2 className="card-title">{post?.title}</h2>
+                            <p>{post?.description}</p>
+                            <p>Likes: {post?.likes_count}</p>
+                           
+                        </div>
+                    </div>
+        </div>
+    );
+};
+
+export default DetailPage;
--- next-level-data-fetching/src/app/posts/page.js
+++ next-level-data-fetching/src/app/posts/page.js
@@ -1,7 +1,13 @@
+import Link from "next/link";
 import React from "react";
 
 const PostsPage = async () => {
-    const res = await fetch("http://localhost:4008/posts");
+    const res = await fetch("http://localhost:4008/posts", {
+        // next: {
+        //     revalidate: 5,
+        // }
+        cache: "no-store",
+    });
     const posts = await res.json();
     // console.log(posts);
 
@@ -13,18 +19,18 @@ const PostsPage = async () => {
 
             <div className="grid grid-cols-3 gap-10 p-10">
                 {posts.map((post) => (
-                    <div
-                        key={post?.id}
-                        className="card bg-base-100 shadow-xl"
-                    >
+                    <div key={post?.id} className="card bg-base-100 shadow-xl">
                         <div className="card-body">
                             <h2 className="card-title">{post?.title}</h2>
                             <p>{post?.description}</p>
                             <p>Likes: {post?.likes_count}</p>
                             <div className="card-actions justify-end">
-                                <button className="btn btn-accent text-white">
-                                    See More
-                                </button>
+                                <Link href={`/posts/${post?.id}`}>
+                                    {" "}
+                                    <button className="btn btn-accent text-white">
+                                        See More
+                                    </button>
+                                </Link>
                             </div>
                         </div>
                     </div>
